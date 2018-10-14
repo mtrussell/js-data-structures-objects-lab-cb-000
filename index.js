7705f00cba229f3cfff7ef73a89c606b03e5cc4b
Matt Trussell <smtrussell@gmail.com>
2018-10-14 21:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
 
 
 function deleteFromDriverByKey(obj, key){
-
+  const newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
 }
 
 
 function destructivelyDeleteFromDriverByKey(obj, key){
 
-} 
+}
